8f3e82e0024ca5cad6ed9bc91bbcfb56de87165a
Pieter Wigboldus <w3n@w3news.org>
2020-03-20 11:59:36 +0100

    Update the typescript interface

--- js/types.d.ts
+++ js/types.d.ts
@@ -1,14 +1,16 @@
-declare interface Array<T> {
-  multisort(key: string, direction: string): Array<T>;
-  multifilter(key: string, find: string, operator: boolean): Array<T>;
-  multikey(key: string): Array<T>;
-  intersect(array: any[], multi: boolean): Array<T>;
-  diff(array: any[], total: boolean): Array<T>;
-  unique(): Array<T>;
-  pushIfNotExists(newValue: string): number;
-  max(): number | undefined;
-  min(): number | undefined;
-  random(): T;
-  summ(): T | undefined;
-  average(): T | undefined;
+declare interface Arr<T> {
+    multisort(key: string, direction: string): Array<T>;
+    multifilter(key: string, find: string, operator: boolean): Array<T>;
+    multikey(key: string): Array<T>;
+    intersect(array: any[], multi: boolean): Array<T>;
+    diff(array: any[], total: boolean): Array<T>;
+    unique: Array<T>;
+    pushIfNotExists(newValue: string): number;
+    pushMultiple(newValue: string): number;
+    pushMultipleIfNotExists(newValue: string): number;
+    max: number | null;
+    min: number | null;
+    random: T;
+    summ: number | null;
+    average: number | null;
 }
