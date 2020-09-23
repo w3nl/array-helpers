13116dec2a2cdd88272f2609fe1b4820d1421a3f
Pieter <w3n@w3news.org>
2020-09-23 20:49:13 +0200

    Refactor multifilter fase 1

--- src/modules/multifilter.js
+++ src/modules/multifilter.js
@@ -1,53 +1,71 @@
 const min = require('./min.js');
 const max = require('./max.js');
 
-module.exports = function multifilter(original, key, find, operator) {
-    return original.filter((item) => {
-        let values = item[key];
+class Match {
+    constructor(find, operator) {
+        this.find = find;
+        this.operator = operator || null;
+    }
+
+    check(value) {
         let returnValue = false;
 
-        if (!values) {
-            return returnValue;
-        }
-
-        values = values.toString().split(',');
-
-        values.forEach((value) => {
-            if (Array.isArray(find)) {
-                if (
-                    find.indexOf(value) < 0 &&
-                    (operator == true || operator == '!=' || operator == '<>')
-                ) {
-                    returnValue = true;
-                } else if (find.indexOf(value) >= 0 && !operator) {
-                    returnValue = true;
-                } else if (value > max(find) && operator == '>') {
-                    returnValue = true;
-                } else if (value >= max(find) && operator == '>=') {
-                    returnValue = true;
-                } else if (value < min(find) && operator == '<') {
-                    returnValue = true;
-                } else if (value <= min(find) && operator == '<=') {
-                    returnValue = true;
-                }
-            } else if (
-                value != find &&
-                (operator == true || operator == '!=' || operator == '<>')
+        if (Array.isArray(this.find)) {
+            if (
+                this.find.indexOf(value) < 0 &&
+                (this.operator == true ||
+                    this.operator == '!=' ||
+                    this.operator == '<>')
             ) {
                 returnValue = true;
-            } else if (value == find && !operator) {
+            } else if (this.find.indexOf(value) >= 0 && !this.operator) {
                 returnValue = true;
-            } else if (value > find && operator == '>') {
+            } else if (value > max(this.find) && this.operator == '>') {
                 returnValue = true;
-            } else if (value >= find && operator == '>=') {
+            } else if (value >= max(this.find) && this.operator == '>=') {
                 returnValue = true;
-            } else if (value < find && operator == '<') {
+            } else if (value < min(this.find) && this.operator == '<') {
                 returnValue = true;
-            } else if (value <= find && operator == '<=') {
+            } else if (value <= min(this.find) && this.operator == '<=') {
                 returnValue = true;
             }
-        });
+        } else if (
+            value != this.find &&
+            (this.operator == true || this.operator == '!=' || this.operator == '<>')
+        ) {
+            returnValue = true;
+        } else if (value == this.find && !this.operator) {
+            returnValue = true;
+        } else if (value > this.find && this.operator == '>') {
+            returnValue = true;
+        } else if (value >= this.find && this.operator == '>=') {
+            returnValue = true;
+        } else if (value < this.find && this.operator == '<') {
+            returnValue = true;
+        } else if (value <= this.find && this.operator == '<=') {
+            returnValue = true;
+        }
 
         return returnValue;
-    });
+    }
+
+    static create(original, key, find, operator) {
+        const matcher = new Match(find, operator);
+
+        return original.filter((item) => {
+            let values = item[key];
+    
+            if (!values) {
+                return false;
+            }
+    
+            values = values.toString().split(',');
+    
+            return values.some(matcher.check.bind(matcher));
+        });
+    }
+}
+
+module.exports = function multifilter(original, key, find, operator) {
+    return Match.create(original, key, find, operator);
 };
